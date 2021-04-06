85c1ec247e67505f1d54ce0734d22fdce34f1f34
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2021-04-06 21:50:23 +0200

    Initial commit

---
+++ src/main/java/io/github/austerzockt/essentialsreloaded/EssentialsReloaded.java
@@ -0,0 +1,17 @@
+package io.github.austerzockt.essentialsreloaded;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EssentialsReloaded extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
