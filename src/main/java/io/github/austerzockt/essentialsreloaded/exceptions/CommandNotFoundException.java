e304ff1a776981f056d245fdba24c1fbf17c2a86
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2021-04-10 21:25:49 +0200

    Added a basic CommandNotFoundException

---
+++ src/main/java/io/github/austerzockt/essentialsreloaded/exceptions/CommandNotFoundException.java
@@ -0,0 +1,8 @@
+package io.github.austerzockt.essentialsreloaded.exceptions;
+
+public class CommandNotFoundException extends RuntimeException {
+    public CommandNotFoundException(String message) {
+        super(message);
+    }
+
+}
